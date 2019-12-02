03af54cacbba14d6640d4f1f5ca46cdf07793683
Dardan Gjonbalaj <xdundun@gmail.com>
2019-12-02 02:44:06 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,5 +1,5 @@
 module Memorable
-
+  module ClassMethods
   def reset_all
     self.all.clear
   end
@@ -7,7 +7,7 @@ module Memorable
    def count
      self.all.count
    end
-
+end
 end #end of module
 
 module InstanceMethods
